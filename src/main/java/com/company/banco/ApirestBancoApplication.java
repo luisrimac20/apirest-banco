221c0576a855af286739608b0036498bf3a13165
luisrimac20 <rimacchavezluis@gmail.com>
2024-01-07 18:40:35 -0500

    Primer Comentario

---
+++ src/main/java/com/company/banco/ApirestBancoApplication.java
@@ -0,0 +1,13 @@
+package com.company.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestBancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/banco/ApirestBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
